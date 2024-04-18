0d404d5e432817321d028c518481cd895ea406d8
lucasliao0403 <60577714+lucasliao0403@users.noreply.github.com>
2024-04-17 20:55:25 -0400

    added audio recorder and option to download

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="bg-bg-dark">
+      <body className="bg-bg-dark text-white">
         <Main />
         <NextScript />
       </body>
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,20 @@
 import Image from "next/image";
-// import { Inter } from "next/font/google";
+import { Inter } from "next/font/google";
+import AudioRecorder from "@/components/AudioRecorder";
+import {useState} from 'react'
 
-// const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+
+
   return (
     <div className="">
-        yup
+        <h1>React Media Recorder</h1>
+        
+        <div className="flex flex-row justify-center">
+           <AudioRecorder />
+        </div>
     </div>
   );
 }
