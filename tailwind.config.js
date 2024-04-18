5dd51b5ada6f2d7128ff32ee18a57df6dbc4a70c
lucasliao0403 <60577714+lucasliao0403@users.noreply.github.com>
2024-04-17 20:35:44 -0400

    initialized background

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,13 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        
       },
+
+      colors: {
+        "bg-dark": "#2e2d57",
+        "bg-light":"#fff5f6",
+      }
     },
   },
   plugins: [],
