afeb2f159f62858be127647c18c7369ffd0476e9
Adam <wangadam019@gmail.com>
2024-04-18 12:42:29 -0400

    updated question list

--- assets/data.js
+++ assets/data.js
@@ -1,19 +1,46 @@
 export const questionSet = [
-    {
-        index: 0,
-        title: "Waterloo Engineering",
-        questions: [
-            "What makes you want to study at Waterloo Engineering?", 
-            "What makes you a unique candidate for Waterloo Engineering?",
-        ]
-    },
-
-    {
-        index: 1,
-        title: "McMaster Engineering",
-        questions: [
-            "What makes you want to study at Mac Engineering?", 
-            "What makes you a unique candidate for Mac Engineering?",
-        ]
-    },
+        {
+            "index": 0,
+            "title": "Waterloo Engineering",
+            "questions": [
+                "What motivated you to choose Waterloo Engineering over other programs?",
+                "Can you describe a project or experience where you demonstrated innovation?",
+                "What are your career aspirations and how does Waterloo Engineering fit into these plans?",
+                "How do you handle challenging situations, especially in collaborative projects?"
+            ]
+        },
+    
+        {
+            "index": 1,
+            "title": "McMaster Engineering",
+            "questions": [
+                "Why are you interested in pursuing your engineering education at McMaster?",
+                "Discuss an engineering challenge you have faced and how you overcame it.",
+                "How do you see yourself contributing to the McMaster Engineering community?",
+                "What skills or experiences make you a strong candidate for this program?"
+            ]
+        },
+    
+        {
+            "index": 2,
+            "title": "University of Toronto Engineering",
+            "questions": [
+                "What aspects of the University of Toronto Engineering program appeal to you the most?",
+                "Describe a time when you had to apply your engineering knowledge to solve a practical problem.",
+                "How do you plan to utilize the resources at U of T Engineering to achieve your professional goals?",
+                "In what ways do you think you can contribute to the diverse environment at U of T?"
+            ]
+        },
+    
+        {
+            "index": 3,
+            "title": "Ryerson Engineering",
+            "questions": [
+                "Why did you choose Ryerson's engineering program?",
+                "Provide an example of a technical problem you solved and the steps you took to solve it.",
+                "What do you expect to gain from your time at Ryerson Engineering?",
+                "How do you plan to engage with the Ryerson community outside of the classroom?"
+            ]
+        }
+    
 ]
